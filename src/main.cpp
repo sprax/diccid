bb3a8e0562d3f63d026b2657e9e089c63b06d3ad
sprax <sprax@comcast.net>
2019-09-03 18:44:43 -0400

    Afrikaans

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+using namespace std;
 
 int main(int argc, char **argv)
 {
-    std::cerr << "Hello World" << std::endl;
-    std::cout << "Howdy Earth" << std::endl;
-    std::cout << "What up WIP" << std::endl;
-    std::cerr << "Zdrasti Orb" << std::endl;
+    cerr << "Hello World" << endl;
+    cout << "Howdy Earth" << endl;
+    cout << "What up WIP" << endl;
+    cerr << "Zdrasti Orb" << endl;
+    cerr << "Hallo, wêreld!" << endl;
 }
